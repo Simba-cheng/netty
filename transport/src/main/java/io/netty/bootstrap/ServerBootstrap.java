1b697cc379ac8e7dd5df47c6f11797d81709adfb
yxcheng <yixincheng@pateo.com.cn>
2022-12-05 14:25:23 +0800

    refactor(code): 源码添加注释、流程图、类图

--- transport/src/main/java/io/netty/bootstrap/ServerBootstrap.java
+++ transport/src/main/java/io/netty/bootstrap/ServerBootstrap.java
@@ -149,16 +149,14 @@ public class ServerBootstrap extends AbstractBootstrap<ServerBootstrap, ServerCh
     void init(Channel channel) {
         /*
             为 NioServerSocketChannel 配置TCP等参数
-
-            newOptionsArray()方法返回的就是 由 serverBootstrap.option 方法添加的参数
+            newOptionsArray 方法返回的就是由 serverBootstrap.option 方法添加的参数
             @see io.netty.bootstrap.AbstractBootstrap.option
          */
         setChannelOptions(channel, newOptionsArray(), logger);
 
         /*
             为 NioServerSocketChannel 配置自定义属性
-
-            newAttributesArray()方法返回的就是 由 serverBootstrap.attr 方法添加的 自定义属性
+            newAttributesArray 方法返回的就是由 serverBootstrap.attr 方法添加的 自定义属性
             @see io.netty.bootstrap.AbstractBootstrap.attr
          */
         setAttributes(channel, newAttributesArray());
@@ -184,18 +182,23 @@ public class ServerBootstrap extends AbstractBootstrap<ServerBootstrap, ServerCh
         p.addLast(new ChannelInitializer<Channel>() {
             @Override
             public void initChannel(final Channel ch) {
-                // 注意：这里的ch和上面的channel是同一个对象,即: NioServerSocketChannel
+                // 注意：这里的 ch 和上面的 channel 是同一个对象,即: NioServerSocketChannel
 
                 // 从 NioServerSocketChannel 中取出 pipeline
                 final ChannelPipeline pipeline = ch.pipeline();
 
-                // 将由 serverBootstrap.handler 配置的handler,添加到 NioServerSocketChannel 的 pipeline 中。
+                /*
+                    为 NioServerSocketChannel 的 pipeline 添加 handler
+                    config.handler 方法返回的 handler 就是由 serverBootstrap.handler 方法配置的
+                    @see io.netty.bootstrap.AbstractBootstrap.handler
+                 */
                 ChannelHandler handler = config.handler();
                 if (handler != null) {
                     pipeline.addLast(handler);
                 }
 
-                // 向 NioServerSocketChannel(AbstractNioChannel) 所属的 EventLoop 提交一个异步任务
+                // 向 NioServerSocketChannel 所属的 NioEventLoop 提交一个异步任务
+                // TODO 疑问,此时 NioServerSocketChannel 还未注册到 NioEventLoop 的 selector上, 此时理论上 ch.eventLoop() 应该是null？
                 ch.eventLoop().execute(new Runnable() {
                     @Override
                     public void run() {
